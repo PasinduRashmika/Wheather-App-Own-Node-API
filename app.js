6aa0e1673a91b60e0391a25d3285bb483c3ea876
rashmikaofficial98@gmail.com <rashmikaofficial98@gmail.com>
2021-08-13 23:36:25 +0530

    Add node moodels

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const request = require("request");
+const http = require("http");
+const bodyParser = require("body-parser");
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname));
+
+//app.listen('view', 'ejs');
+
+
+
+
+app.get('/', function(req, res){
+   res.sendFile(__dirname+"/index.html");
+   console.log("adbadbv1");
+
+});
+
+app.post("/",function(req,res){
+    const city=req.body.cityName;
+    //const city='matugama';
+    const API='99e75fc860bfca409438c4f8ce3cb080';
+    const url='http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid='+API;
+
+
+    http.get(url, function(response){
+        response.on("data",function(data){
+            const wheatheData=JSON.parse(data);
+            const temps=wheatheData.main.temp;
+            console.log(wheatheData);
+            console.log(temps);
+            res.render("wheather");
+        })
+    })
+
+})
+
+app.listen(3000,function () {
+    console.log("Server is running on port 3000");
+  });
